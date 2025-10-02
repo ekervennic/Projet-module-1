bfd0e8604bbdcf680703d1f11bbc5201068d9fc4
Elena <elenakervennic@gmail.com>
2025-10-02 13:12:53 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,96 @@
+
+# TODO: Create an integer variable named 'age' with your age
+
+# TODO: Create a float variable named 'height' with your height in meters
+
+# TODO: Create a string variable named 'name' with your name
+
+# TODO: Create a boolean variable named 'is_student' and set it to True
+
+# Print all variables and their types
+# Example: print(f"Age: {age}, Type: {type(age)}")
+
+age = 22
+height = 1.65
+name = "Elena"
+is_student = True
+print(is_student)
+# TODO: Create two variables 'a' and 'b' with numeric values
+
+# TODO: Perform addition, subtraction, multiplication, and division of a and b
+# Print the results of each operation
+
+# TODO: Calculate the remainder of a divided by b using the modulo operator
+a = 34
+b = 56
+print(a - b)
+print(a + b)
+print(a / b)
+print(a * b)
+print(a % b)
+print(a ** b)
+# TODO: Create two string variables 'first_name' and 'last_name'
+
+# TODO: Concatenate the two names to create a 'full_name' variable
+
+# TODO: Print the full name in uppercase
+
+# TODO: Print the length of the full name
+
+# TODO: Create a string with multiple words and split it into a list
+first_name = "Elena"
+last_name = "Kervennic"
+full_name = first_name + " " + last_name
+print(full_name)
+print(len(full_name))
+name_parts = full_name.split()
+print(full_name.upper())
+print(name_parts)
+# TODO: Convert a string containing a number to an integer
+
+# TODO: Convert a float to an integer
+
+# TODO: Convert an integer to a float
+
+# TODO: Convert a number (integer or float) to a string
+
+# Print all converted values and their new types
+num_str = "42"
+num_int = int(num_str)
+print(num_int)
+
+# Convert float to int
+num_float = 4.8
+num_int = int(num_float)
+print(num_int)
+
+# Convert int to float
+num_float = float(num_int)
+print(num_float)
+
+# Convert numbers to strings
+str_int = str(num_int)
+str_float = str(num_float)
+print(str_int)
+print(str_float)
+
+# TODO: Create two boolean variables
+
+# TODO: Perform AND, OR, and NOT operations on these variables
+# Print the results
+
+# TODO: Create two numeric variables and use comparison operators
+# (>, <, >=, <=, ==, !=) to compare them
+# Print the results of these comparisons
+raining = True
+beautiful = False
+print(raining and beautiful)
+print(raining or beautiful)
+print(not beautiful)
+print(not raining)
+
+a = 22
+b = 45
+print(a > b)
+print(a == b)
+print(a != b)
